b17b4b12c21e6221acc9f889a496c3560c678b76
Dosztosz <dosztosz@int.pl>
2022-08-18 09:29:15 +0200

    Update head.php

--- addons/head.php
+++ addons/head.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="assets/css/style.css?version=5" rel="stylesheet" media="screen">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-<script src="assets/js/script.js"></script>
+<script src="assets/js/script.js?version=1"></script>
 <link rel="shortcut icon" href="favicon.ico">
 <script src="assets/js/jquery-3.6.0.min.js"></script>
 <script src="https://kit.fontawesome.com/8d0eb52cc9.js" crossorigin="anonymous"></script>
