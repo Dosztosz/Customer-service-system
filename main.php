22bbc7029604f6ad9a7d69520492c1068b6f2484
Dosztosz <dosztosz@int.pl>
2022-08-22 14:31:14 +0200

    update

--- main.php
+++ main.php
@@ -75,6 +75,47 @@
                     <input class="contactform-input input" type="text" id="input-data" placeholder="Cm" />
                     <span id="result"></span>
                 </div>
+                <script>
+                    function calculator(data){
+                    var cmCal = 0.393700787;
+                    var cal = 2.54;
+                    var stopa = 30.48;
+                    var cmStopa = 0.032808399;
+                    var funt = 2.20462262;
+                    var kilogram = 0.45359237;
+                    var input = document.getElementById("input-data").value;
+
+
+                    switch(data){
+                        case 'cmCale':
+                            var calculate = input * cmCal;
+                            var end = ' "';
+                            break;
+                        case 'caleCm':
+                            var calculate = input * cal;
+                            var end = ' cm';
+                            break;
+                        case 'cmStopy':
+                            var calculate = input * cmStopa;
+                            var end = ' stóp';
+                            break;
+                        case 'stopyCm':
+                            var calculate = input * stopa;
+                            var end = ' cm';
+                            break;
+                        case 'kgFunt':
+                            var calculate = input * funt;
+                            var end = ' funtów';
+                            break;
+                        case 'funtKg':
+                            var calculate = input * kilogram;
+                            var end = ' kg'
+                            break;
+                    }
+                    var result = (calculate);
+                    document.getElementById("result").innerHTML = result+end;
+                }
+                </script>
                     <!--pole tekstowe-->
                 <h3>pole tekstowe</h3>
                 <textarea rows="25" cols="80"></textarea>
