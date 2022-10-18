f613a7e6d8ecd90103a7e68587ebbe12e328a1ab
Tomasz Dębosz <dosztosz@int.pl>
2022-10-18 07:24:50 +0200

    up

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	define ('DB_HOST','127.0.0.1');
-	define ('DB_USER', 'root');
-	define ('DB_PASSWORD', '');
-	define ('DB_NAME', 'customer_service');
+	define ('DB_HOST','mariadb106.server723887.nazwa.pl');
+	define ('DB_USER', 'server723887_CustomerService');
+	define ('DB_PASSWORD', 'A76bvo7856b287t!@#b71');
+	define ('DB_NAME', 'server723887_CustomerService');
 	$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 	$conn->set_charset("utf8");
 ?>
